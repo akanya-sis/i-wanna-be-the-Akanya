8c59695f0e1cac651be60f98da3601eb7973682a
akanya-sis <148517081+akanya-sis@users.noreply.github.com>
2025-03-11 16:12:12 +0900

    Update collisions.js

--- collisions.js
+++ collisions.js
@@ -1,7 +1,7 @@
 // 当たり判定用の矩形を取得
 function getCollisionRect(player) {
     const offsetX = (player.width - player.collisionWidth) / 2;
-    const offsetY = (player.height - player.collisionHeight) - 1;
+    const offsetY = (player.height - player.collisionHeight) - 5;
     return {
         x: player.x + offsetX,
         y: player.y + offsetY,
